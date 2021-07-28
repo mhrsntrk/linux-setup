1dbd6b8484d3b8d221754d6d0c2ce78c4b6fb61e
mhrsntrk <m@mhrsntrk.com>
2021-07-28 19:01:32 +0300

    commit

---
+++ userSetup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install neovim zsh bat fd-find figlet   
+
+#Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
