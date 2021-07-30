18a4c51755fea9bbeac0ce4a1a5d0e004794050c
mhrsntrk <m@mhrsntrk.com>
2021-07-30 11:57:09 +0300

    commit

--- userSetup.sh
+++ userSetup.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install neovim zsh bat fd-find figlet   
 
-#Install oh-my-zsh
+#Install zsh and oh-my-zsh and copy configuration file
+sudo apt-get install zsh    
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+cp dotfiles/.zshrc .
+
+# Install neovim and plugin manager and copy configuration file
+sudo apt-get install neovim
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+cp -R dotfiles/.config .
+
+#Install other tools
+sudo apt-get install bat
+
+sudo apt-get install fd-find
+
+sudo apt-get install figlet
+
+# Copy other configuration files
+
+cp /dotfiles/.tmux.conf .
+
+cp /dotfiles/.gitconfig .
+
+cp /dotfiles/Modular.flf /usr/share/figlet
 
 
