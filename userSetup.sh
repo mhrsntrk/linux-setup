780fcf7642d1279d58edf7b7c0b47e3ec89e6606
mhrsntrk <m@mhrsntrk.com>
2021-07-30 15:55:02 +0300

    commit

--- userSetup.sh
+++ userSetup.sh
@@ -36,3 +36,5 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+mkdir ~/developer
