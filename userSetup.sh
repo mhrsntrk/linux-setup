2bc967c84a98868bb283f195fc9e32afaaf35d67
mhrsntrk <m@mhrsntrk.com>
2021-07-30 14:13:39 +0300

    commit

--- userSetup.sh
+++ userSetup.sh
@@ -2,20 +2,13 @@
 
 sudo apt-get update
 
-#Install zsh and oh-my-zsh and copy configuration file
-sudo apt-get install zsh    
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-cp dotfiles/.zshrc .
-
 # Install neovim and plugin manager and copy configuration file
 sudo apt-get install neovim
 
+sudo cp -R dotfiles/.config .
+
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-cp -R dotfiles/.config .
 
 #Install other tools
 sudo apt-get install bat
@@ -30,7 +23,11 @@ cp /dotfiles/.tmux.conf .
 
 cp /dotfiles/.gitconfig .
 
-cp /dotfiles/Modular.flf /usr/share/figlet
+sudo cp /dotfiles/Modular.flf /usr/share/figlet
 
-# Reload zshrc file
-source .zshrc
+#Install zsh and oh-my-zsh and copy configuration file
+sudo apt-get install zsh
+
+cp dotfiles/.zshrc .
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
