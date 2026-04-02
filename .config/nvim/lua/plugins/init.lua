c886250ef4ec7721350fa6bd1d97b32c96446093
mhrsntrk <m@mhrsntrk.com>
2026-04-02 17:19:18 +0200

    fix: add indent-blankline highlight groups to prevent nvim errors

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -24,4 +24,13 @@ return {
       },
     },
   },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    opts = function(_, opts)
+      -- Ensure highlight groups exist before plugin loads
+      vim.api.nvim_set_hl(0, "IblChar", { fg = "#3b3b3b" })
+      vim.api.nvim_set_hl(0, "IblScopeChar", { fg = "#5c5c5c" })
+      return opts
+    end,
+  },
 }
