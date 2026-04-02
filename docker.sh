3bf387ab569a2c4874cb8e761bbf8e9c6b3cde78
mhrsntrk <m@mhrsntrk.com>
2026-04-02 16:16:25 +0200

    fix: always re-download Docker GPG key to ensure validity

--- docker.sh
+++ docker.sh
@@ -11,10 +11,8 @@ main() {
   ensure_packages ca-certificates curl gnupg lsb-release
   sudo install -m 0755 -d /etc/apt/keyrings
 
-  if [[ ! -f /etc/apt/keyrings/docker.asc ]]; then
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.asc
-    sudo chmod a+r /etc/apt/keyrings/docker.asc
-  fi
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.asc
+  sudo chmod a+r /etc/apt/keyrings/docker.asc
 
   printf '%s\n' 'deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu noble stable' | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 
